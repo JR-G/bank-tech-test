2ef0d5864c0a5535819d7fc0075a5ca501ddac3c
JRG <47917431+JR-G@users.noreply.github.com>
2020-10-05 13:42:21 +0100

    Test: Red - Withdraw method decreases balance

--- spec/bank_interface_spec.rb
+++ spec/bank_interface_spec.rb
@@ -4,7 +4,7 @@ describe Bank do
   let(:account) { Bank.new }
   
   describe '#deposit' do
-    it 'increases balance to 500 after a deposit of 500' do
+    it 'increases balance after a deposit' do
       account.deposit(500)
       expect(account.balance).to eq 500
     end
@@ -12,5 +12,13 @@ describe Bank do
     it 'doesn\'t allow a debit/negative deposit' do
       expect { subject.deposit(-100) }.to raise_error "This isn't a valid deposit"
     end 
+  end
+
+  describe '#withdraw' do
+    it 'decreases balance after a withdrawal' do
+      account_with_balance = Bank.new(100)
+      account_with_balance.withdraw(50)
+      expect(account.balance).to eq 50
+    end
   end
 end
