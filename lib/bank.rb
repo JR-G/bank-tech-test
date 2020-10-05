bf936d7c003664efa1152c35475b36fec6575100
JRG <47917431+JR-G@users.noreply.github.com>
2020-10-05 15:58:25 +0100

    Build: Green - Passed activity logger

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,22 +1,25 @@
 class Bank
 
-  attr_reader :balance
+  attr_reader :balance, :activity
 
   def initialize(balance = 0)
     @balance = balance
+    @activity = []
   end
 
-  def deposit(amount)
+  def deposit(amount, date = Time.new.strftime("%d/%m/%Y"))
     fail "This isn't a valid deposit" if negative?(amount)
 
     @balance += amount
+    @activity << [date, amount, nil, balance]
   end
 
-  def withdraw(amount)
+  def withdraw(amount, date = Time.new.strftime("%d/%m/%Y"))
     fail "Invalid value" if negative?(amount)
     fail "Unable to enter overdraft" if negative?(balance - amount)
 
     @balance -= amount
+    @activity << [date, nil, amount, balance]
   end
 
   private
