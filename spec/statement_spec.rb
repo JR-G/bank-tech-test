4d4c8b2526d9fb6e74506b6c18270a6c6ecb642a
JRG <47917431+JR-G@users.noreply.github.com>
2020-10-05 16:30:26 +0100

    Test: Red - Print out an empty statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,8 +1,15 @@
 require 'statement'
 
 describe Statement do
-  describe '#print' do
-    it 'will return a statement' do
-    end
+  let(:account) { Bank.new }
+  let(:date) { Time.new.strftime("%d/%m/%Y") }
+
+  it 'prints out a statement' do
+    expect(subject.print_out).to eq "date || credit || debit || balance"
+  end
+
+  it 'prints out activity' do
+    # account.deposit(500)
+    # account.withdraw(100)
   end
 end
