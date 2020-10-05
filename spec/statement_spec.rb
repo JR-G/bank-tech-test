14a380107047e9c1a6b61a33bda3351958656583
JRG <47917431+JR-G@users.noreply.github.com>
2020-10-05 19:00:37 +0100

    Test: Red - Can print out another statement after a new transaction

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -13,4 +13,11 @@ describe Statement do
     account.withdraw(100)
     expect { subject.print_out(account.activity) }.to output("date || credit || debit || balance\n #{date} ||  || 100 || 400\n #{date} || 500 ||  || 500\n").to_stdout 
   end
+
+  it 'can print out a statement twice' do
+    account.deposit(100)
+    subject.print_out(account.activity)
+    account.withdraw(50)
+    expect { subject.print_out(account.activity) }.to output("date || credit || debit || balance\n #{date} ||  || 50 || 50\n #{date} || 100 ||  || 100\n").to_stdout 
+  end
 end
