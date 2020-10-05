f26555bfdb18318456c6187a5150dbcc815df6f1
JRG <47917431+JR-G@users.noreply.github.com>
2020-10-05 17:57:43 +0100

    Build: Green - Print out account activity

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,8 +1,16 @@
-require 'bank'
+require_relative 'bank'
 
 class Statement
-  def print_out
-    header
+  
+  def print_out(values = false)
+      if values
+        values.map! { |entry| entry.join(" || ") }
+        reversed_elements = values.reverse.join("\n ")
+        statement = "#{header}\n #{reversed_elements}"
+        puts statement
+        return statement
+      else header
+      end
   end
 
   private
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -5,12 +5,12 @@ describe Statement do
   let(:date) { Time.new.strftime("%d/%m/%Y") }
 
   it 'prints out a statement' do
-    expect(subject.print_out).to eq "date || credit || debit || balance"
+    expect(subject.print_out()).to eq "date || credit || debit || balance"
   end
 
   it 'prints out activity' do
     account.deposit(500)
     account.withdraw(100)
-    expect(subject.print_out).to eq "date || credit || debit || balance\n #{date} || || 100 || 400\n #{date} || 500 || || 500"
+    expect(subject.print_out(account.activity)).to eq "date || credit || debit || balance\n #{date} ||  || 100 || 400\n #{date} || 500 ||  || 500"
   end
 end
