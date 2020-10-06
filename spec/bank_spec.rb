271ed68c64ade272d1c515ed64b0dee00c046a24
JRG <47917431+JR-G@users.noreply.github.com>
2020-10-06 17:07:14 +0100

    Test: Green - changed a balance checker test

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,23 +5,22 @@ describe Bank do
   let(:account_with_balance) { Bank.new(100) }
   let(:date) { Time.new.strftime("%d/%m/%Y") }
   
+  
+  it 'balance is correctly summed after deposits and withdrawals' do
+    account.deposit(1000)
+    account.deposit(2000)
+    account.withdraw(500)
+    expect(account.balance).to eq 2500
+  end
+  
+  
   describe '#deposit' do
-    it 'increases balance after a deposit' do
-      account.deposit(500)
-      expect(account.balance).to eq 500
-    end
-
     it 'doesn\'t allow a debit/negative deposit' do
       expect { subject.deposit(-100) }.to raise_error "This isn't a valid deposit"
     end 
   end
 
   describe '#withdraw' do
-    it 'decreases balance after a withdrawal' do
-      account_with_balance.withdraw(50)
-      expect(account_with_balance.balance).to eq 50
-    end
-
     it 'prevents the account becoming overdrawn' do
       expect { account.withdraw(50) }.to raise_error "Unable to enter overdraft"
     end
